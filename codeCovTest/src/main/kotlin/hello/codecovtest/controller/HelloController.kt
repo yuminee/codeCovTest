6ba7b2b872927fb32db0ebbf7b10991b8e00ab32
yuminee <ayuminee2@gmail.com>
2024-05-03 15:19:04 +0900

    test

--- codeCovTest/src/main/kotlin/hello/codecovtest/controller/HelloController.kt
+++ codeCovTest/src/main/kotlin/hello/codecovtest/controller/HelloController.kt
@@ -1,10 +1,15 @@
 package hello.codecovtest.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
     @GetMapping("/message")
-    fun index(): String = "Hello"
+    fun get(): String = "Hello"
+
+    @PostMapping("/message")
+    fun post(): String = "Hello"
+
 }
